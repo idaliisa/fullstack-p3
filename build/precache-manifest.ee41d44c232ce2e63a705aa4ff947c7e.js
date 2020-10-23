8b1fadd23ef544d82180afe88d221140556a0a52
idaliisa <idaliisa@gmail.com>
2020-10-23 22:22:12 +0300

    part3: added frontend

---
+++ build/precache-manifest.ee41d44c232ce2e63a705aa4ff947c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1258f5981ea095285f47b1863998e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb3bf2a8613e7a05705b",
+    "url": "/static/css/main.f7e7aee6.chunk.css"
+  },
+  {
+    "revision": "50b2e459d25667e2841c",
+    "url": "/static/js/2.37c0a557.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.37c0a557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb3bf2a8613e7a05705b",
+    "url": "/static/js/main.8e106230.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
